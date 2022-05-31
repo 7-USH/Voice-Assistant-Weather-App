e0d0cdf9fc70529c9bd0258041d86d18ddc90f21
Tushar <tush1245@gmail.com>
2022-05-31 05:34:08 +0530

    Weather Map | added

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -2,11 +2,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:weather_app/models/location.dart';
+import 'package:weather_app/models/weather.dart';
 import 'package:weather_app/models/weatherModel.dart';
+import 'package:weather_app/widgets/point_weather.dart';
+
+const double pinInvisible = -300;
+const double pinVisible = -20;
 
 class MapScreen extends StatefulWidget {
-  MapScreen({Key? key, required this.coords, required this.model}) : super(key: key);
+  MapScreen({Key? key, required this.coords, required this.model})
+      : super(key: key);
   List<double> coords;
   WeatherModel model;
   @override
@@ -20,7 +29,14 @@ class _MapScreenState extends State<MapScreen> {
   late double lon;
   Set<Marker> _markers = Set<Marker>();
   late BitmapDescriptor sourceMarker;
-  bool mapTap = false;
+  bool sourceTap = false;
+  GeoCoder coder = GeoCoder();
+  WeatherDetails details = WeatherDetails();
+
+  late String cityName;
+  late int temp;
+  late int id;
+  bool isLoading = false;
 
   void setCustomMarker() async {
     sourceMarker = await BitmapDescriptor.fromAssetImage(
@@ -34,6 +50,13 @@ class _MapScreenState extends State<MapScreen> {
     lat = widget.coords[0];
     lon = widget.coords[1];
     setCustomMarker();
+    getCurrentLocationDetails();
+  }
+
+  void getCurrentLocationDetails() {
+    cityName = widget.model.name!;
+    temp = widget.model.main!.temp!.toInt();
+    id = widget.model.weather!.id!.toInt();
   }
 
   void setMapStyle(String mapStyle) {
@@ -55,10 +78,21 @@ class _MapScreenState extends State<MapScreen> {
         position: LatLng(lat, lon),
         icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
         onTap: () {
-          
+          myBottomModalSheet(id, cityName, temp);
         },
       ));
     });
+  }
+
+  myBottomModalSheet(int id1, String cityname1, int temp1) {
+    showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        context: context,
+        constraints: BoxConstraints(maxHeight: 300),
+        builder: (_) {
+          return PointWeather(id: id1, name: cityname1, temp: temp1);
+        });
   }
 
   static const CameraPosition _initialCameraPosition =
@@ -69,40 +103,81 @@ class _MapScreenState extends State<MapScreen> {
     var _finalCameraPosition =
         CameraPosition(target: LatLng(lat, lon), zoom: 10);
 
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: GoogleMap(
-        myLocationButtonEnabled: false,
-        onTap: (location) {
-          setState(() {
-            _markers.add(Marker(
-                markerId: MarkerId("Source-2"),
-                position: LatLng(location.latitude, location.longitude),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueRed),
-                onTap: () {
-                  _markers.remove(_markers.elementAt(1));
-                  setState(() {});
-                }));
-          });
-        },
-        markers: _markers,
-        compassEnabled: false,
-        mapToolbarEnabled: false,
-        zoomControlsEnabled: true,
-        initialCameraPosition: _initialCameraPosition,
-        onMapCreated: (GoogleMapController controller) {
-          _controller = controller;
-          showPinsOnMap();
-          Future.delayed(Duration(seconds: 1)).then((value) {
-            _controller.animateCamera(
-                CameraUpdate.newCameraPosition(_finalCameraPosition));
-          });
-          isMapCreated = true;
-          setState(() {
-            changeMapMode();
-          });
-        },
-      ),
+      body: Stack(children: [
+        GoogleMap(
+          myLocationButtonEnabled: false,
+          zoomGesturesEnabled: true,
+          onTap: (location) {
+            setState(() {
+              _markers.add(Marker(
+                  markerId: MarkerId("Source-2"),
+                  position: LatLng(location.latitude, location.longitude),
+                  icon: BitmapDescriptor.defaultMarkerWithHue(
+                      BitmapDescriptor.hueRed),
+                  onTap: () {
+                    coder.getPlacemarks(
+                        [location.latitude, location.longitude]).then((value) {
+                      print(value);
+                      if (value != "") {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        details.getData("Weather in $value").then((v) {
+                          if (v != null) {
+                            WeatherModel model = WeatherModel.fromJson(v);
+                            myBottomModalSheet(
+                                model.weather!.id!.toInt(),
+                                model.name.toString(),
+                                model.main!.temp!.toInt());
+                            setState(() {
+                              isLoading = false;
+                            });
+                          } else {
+                            setState(() {
+                              isLoading = false;
+                            });
+                          }
+                        });
+                      } else {
+                        Fluttertoast.showToast(msg: "There was some error");
+                      }
+                    });
+                  }));
+            });
+          },
+          markers: _markers,
+          compassEnabled: false,
+          mapToolbarEnabled: false,
+          zoomControlsEnabled: false,
+          initialCameraPosition: _initialCameraPosition,
+          onMapCreated: (GoogleMapController controller) {
+            _controller = controller;
+            showPinsOnMap();
+            Future.delayed(Duration(seconds: 1)).then((value) {
+              _controller.animateCamera(
+                  CameraUpdate.newCameraPosition(_finalCameraPosition));
+            });
+            isMapCreated = true;
+            setState(() {
+              changeMapMode();
+            });
+          },
+        ),
+        isLoading
+            ? Positioned(
+                bottom: 20,
+                right: 20,
+                child: Container(
+                  child: LoadingAnimationWidget.fallingDot(
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                ))
+            : Container()
+      ]),
     );
   }
 }
--- lib/widgets/point_weather.dart
+++ lib/widgets/point_weather.dart
@@ -1,15 +1,129 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_const_constructors_in_immutables, must_be_immutable, unused_local_variable
+
 import 'package:flutter/material.dart';
+import 'package:weather_app/constants/colors.dart';
+import 'package:weather_app/constants/fonts.dart';
+import 'package:weather_app/models/weather_image.dart';
+import 'dart:math' as math;
 
 class PointWeather extends StatefulWidget {
-  PointWeather({Key? key}) : super(key: key);
+  PointWeather(
+      {Key? key, required this.id, required this.name, required this.temp})
+      : super(key: key);
+  String name;
+  int temp;
+  int id;
 
   @override
   State<PointWeather> createState() => _PointWeatherState();
 }
 
-class _PointWeatherState extends State<PointWeather> {
+class _PointWeatherState extends State<PointWeather>
+    with SingleTickerProviderStateMixin {
+  String image = "assets/weather/Zaps.png";
+  late Animation<double> _animation;
+  late AnimationController _controller;
+
+  int celsiusCalc(int kelvin) {
+    return kelvin - 273;
+  }
+
+  void setRotation(int deg) {
+    final angle = deg * math.pi / 180;
+    _animation = Tween<double>(begin: 0, end: angle).animate(_controller);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    setRotation(-90);
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+          color: blobColor,
+          gradient: LinearGradient(
+              end: Alignment.bottomRight,
+              begin: Alignment.topLeft,
+              colors: [
+                bgColor,
+                darkBgColor,
+              ]),
+          borderRadius: BorderRadius.circular(30)),
+      child: Column(
+        children: [
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 55),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.name,
+                  style: appText(
+                      color: Colors.white, size: 30, weight: FontWeight.normal),
+                ),
+                AnimatedBuilder(
+                  animation: _animation,
+                  builder: (BuildContext context, Widget? child) =>
+                      Transform.rotate(angle: _animation.value, child: child),
+                  child: Icon(
+                    Icons.swipe_down_rounded,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                imagePath(id: widget.id),
+                scale: 5,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Today, ",
+                    style: botFont(
+                        color: Colors.white,
+                        size: 25,
+                        weight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    "${celsiusCalc(widget.temp)}°",
+                    style: botFont(
+                        color: Colors.white, size: 60, weight: FontWeight.bold),
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
